561451912bfac0abaaadebd75ccbf91b098d80d5
Hemanth Varupula <varupulahemanth@gmailo.com>
2025-05-30 10:25:26 +0530

    episode4 added

---
+++ src/Episode4/App.js
@@ -0,0 +1,381 @@
+import reactDom from "react-dom/client";
+
+const Header=()=>{
+    return(
+        <>
+            <div className="header">
+                <div className="logo"><img className ="logo1"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtNDCEKa-eUQfHSD-KfVcH5IwB95c1w-tmgA&s"/></div>
+                <div className="nav-items">
+                    <ul className="item">
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Contact us</li>
+                        <li>Cart</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+const resobj=
+[
+    {
+              "id": "63048",
+              "name": "Sharief Bhai Biryani",
+              "cloudinaryImageId": "FOOD_CATALOG/IMAGES/CMS/2024/11/21/cdad749f-165b-4aaa-b6b9-1c6f966494b0_a8628830-65ec-43e0-8455-9f4a2ae41001.jpg",
+              "locality": "Frazer Town",
+              "areaName": "Frazer Town",
+              "costForTwo": "₹400 for two",
+              "cuisines": [
+                "Biryani",
+                "South Indian",
+                "Kebabs",
+                "Ramzan Special",
+                "Sweets",
+                "Beverages"
+              ],
+              "avgRating": 4.3,
+              "parentId": "469102",
+              "avgRatingString": "4.3",
+              "totalRatingsString": "47K+",
+              "promoted": true,
+              "adTrackingId": "cid=29459298~p=1~adgrpid=29459298#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=63048~plpr=COLLECTION~eid=872d07a2-a329-4dcd-a2d8-16df5a5a248b~srvts=1748553901744~collid=83639",
+              "sla": {
+                "deliveryTime": 35,
+                "lastMileTravel": 7.5,
+                "serviceability": "SERVICEABLE",
+                "slaString": "30-35 mins",
+                "lastMileTravelString": "7.5 km",
+                "iconType": "ICON_TYPE_EMPTY"
+              },
+              "availability": {
+                "nextCloseTime": "2025-05-30 03:00:00",
+                "opened": true
+              },
+              "badges": {},
+              "isOpen": true,
+              "type": "F",
+              "badgesV2": {
+                "entityBadges": {
+                  "textBased": {},
+                  "imageBased": {},
+                  "textExtendedBadges": {}
+                }
+              },
+              "aggregatedDiscountInfoV3": {
+                "header": "₹200 OFF",
+                "subHeader": "ABOVE ₹799",
+                "discountTag": "FLAT DEAL"
+              },
+              "orderabilityCommunication": {
+                "title": {},
+                "subTitle": {},
+                "message": {},
+                "customIcon": {},
+                "commsStyling": {}
+              },
+              "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                  "lottie": {},
+                  "video": {}
+                }
+              },
+              "reviewsSummary": {},
+              "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+              "restaurantOfferPresentationInfo": {},
+              "externalRatings": {
+                "aggregatedRating": {
+                  "rating": "4.0",
+                  "ratingCount": "2.5K+"
+                },
+                "source": "GOOGLE",
+                "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+              },
+              "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+              "campaignId": "29459298"
+ },
+ {
+              "id": "186107",
+              "name": "The Biryani Life",
+              "cloudinaryImageId": "woekdmnel2wvc9dpbmms",
+              "locality": "Rashtriya Vidyalaya Rd",
+              "areaName": "Jayanagar",
+              "costForTwo": "₹250 for two",
+              "cuisines": [
+                "Biryani",
+                "Mughlai",
+                "Lucknowi",
+                "Hyderabadi",
+                "Kebabs",
+                "Desserts",
+                "Beverages"
+              ],
+              "avgRating": 3.9,
+              "parentId": "8496",
+              "avgRatingString": "3.9",
+              "totalRatingsString": "860",
+              "sla": {
+                "deliveryTime": 22,
+                "lastMileTravel": 3,
+                "serviceability": "SERVICEABLE",
+                "slaString": "20-25 mins",
+                "lastMileTravelString": "3.0 km",
+                "iconType": "ICON_TYPE_EMPTY"
+              },
+              "availability": {
+                "nextCloseTime": "2025-05-30 03:00:00",
+                "opened": true
+              },
+              "badges": {},
+              "isOpen": true,
+              "type": "F",
+              "badgesV2": {
+                "entityBadges": {
+                  "textExtendedBadges": {},
+                  "textBased": {},
+                  "imageBased": {}
+                }
+              },
+              "aggregatedDiscountInfoV3": {
+                "header": "ITEMS",
+                "subHeader": "AT ₹159"
+              },
+              "orderabilityCommunication": {
+                "title": {},
+                "subTitle": {},
+                "message": {},
+                "customIcon": {},
+                "commsStyling": {}
+              },
+              "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                  "lottie": {},
+                  "video": {}
+                }
+              },
+              "reviewsSummary": {},
+              "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+              "restaurantOfferPresentationInfo": {},
+              "externalRatings": {
+                "aggregatedRating": {
+                  "rating": "--"
+                }
+              },
+              "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+{
+              "id": "1037841",
+              "name": "Mr Phillys American Cheeseburgers",
+              "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/4/22/5ccdabd4-2357-4ebd-823a-6092e8491166_1037841 (1).jpg",
+              "locality": "Coles Road",
+              "areaName": "Frazer Town",
+              "costForTwo": "₹500 for two",
+              "cuisines": [
+                "Burgers",
+                "American",
+                "Biryani",
+                "Fast Food"
+              ],
+              "avgRating": 4.5,
+              "parentId": "516779",
+              "avgRatingString": "4.5",
+              "totalRatingsString": "568",
+              "promoted": true,
+              "adTrackingId": "cid=29597616~p=2~adgrpid=29597616#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=1037841~plpr=COLLECTION~eid=6b227a7f-5b4b-422f-958d-a0908537f0e1~srvts=1748553901744~collid=83639",
+              "sla": {
+                "deliveryTime": 37,
+                "lastMileTravel": 6.3,
+                "serviceability": "SERVICEABLE",
+                "slaString": "35-40 mins",
+                "lastMileTravelString": "6.3 km",
+                "iconType": "ICON_TYPE_EMPTY"
+              },
+              "availability": {
+                "nextCloseTime": "2025-05-30 04:00:00",
+                "opened": true
+              },
+              "badges": {},
+              "isOpen": true,
+              "type": "F",
+              "badgesV2": {
+                "entityBadges": {
+                  "imageBased": {},
+                  "textExtendedBadges": {},
+                  "textBased": {}
+                }
+              },
+              "aggregatedDiscountInfoV3": {
+                "header": "60% OFF",
+                "subHeader": "UPTO ₹120",
+                "logoCtx": {
+                  "text": "BENEFITS"
+                }
+              },
+              "orderabilityCommunication": {
+                "title": {},
+                "subTitle": {},
+                "message": {},
+                "customIcon": {},
+                "commsStyling": {}
+              },
+              "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                  "lottie": {},
+                  "video": {}
+                }
+              },
+              "reviewsSummary": {},
+              "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+              "isNewlyOnboarded": true,
+              "restaurantOfferPresentationInfo": {},
+              "externalRatings": {
+                "aggregatedRating": {
+                  "rating": "--"
+                }
+              },
+              "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+              "campaignId": "29597616"
+            },
+            {
+              "id": "93498",
+              "name": "Behrouz Biryani",
+              "cloudinaryImageId": "a4ffed13eb197c6df43dfe1c756560e5",
+              "locality": "Rashtriya Vidyalaya Rd",
+              "areaName": "Jayanagar",
+              "costForTwo": "₹500 for two",
+              "cuisines": [
+                "Biryani",
+                "North Indian",
+                "Kebabs",
+                "Mughlai",
+                "Lucknowi",
+                "Hyderabadi",
+                "Desserts",
+                "Beverages"
+              ],
+              "avgRating": 4.2,
+              "parentId": "1803",
+              "avgRatingString": "4.2",
+              "totalRatingsString": "2.3K+",
+              "sla": {
+                "deliveryTime": 25,
+                "lastMileTravel": 3,
+                "serviceability": "SERVICEABLE",
+                "slaString": "25-30 mins",
+                "lastMileTravelString": "3.0 km",
+                "iconType": "ICON_TYPE_EMPTY"
+              },
+              "availability": {
+                "nextCloseTime": "2025-05-30 03:00:00",
+                "opened": true
+              },
+              "badges": {
+                "textExtendedBadges": [
+                  {
+                    "iconId": "guiltfree/GF_Logo_android_3x",
+                    "shortDescription": "options available",
+                    "fontColor": "#7E808C"
+                  }
+                ]
+              },
+              "isOpen": true,
+              "type": "F",
+              "badgesV2": {
+                "entityBadges": {
+                  "textBased": {},
+                  "imageBased": {},
+                  "textExtendedBadges": {
+                    "badgeObject": [
+                      {
+                        "attributes": {
+                          "shortDescription": "options available",
+                          "fontColor": "#7E808C",
+                          "iconId": "guiltfree/GF_Logo_android_3x",
+                          "description": ""
+                        }
+                      }
+                    ]
+                  }
+                }
+              },
+              "aggregatedDiscountInfoV3": {
+                "header": "ITEMS",
+                "subHeader": "AT ₹99"
+              },
+              "orderabilityCommunication": {
+                "title": {},
+                "subTitle": {},
+                "message": {},
+                "customIcon": {},
+                "commsStyling": {}
+              },
+              "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                  "lottie": {},
+                  "video": {}
+                }
+              },
+              "reviewsSummary": {},
+              "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+              "restaurantOfferPresentationInfo": {},
+              "externalRatings": {
+                "aggregatedRating": {
+                  "rating": "--"
+                }
+              },
+              "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+            },
+        ]
+const Reataurentcard=(props)=>{
+    const {resData}=props;
+    //Destructure on the fly
+    const {cloudinaryImageId,name,cuisines,avgRating,sla}=resData;
+    // console.log(props)
+    return (
+        <>
+           <div className="res-card">
+             <img className="res-img" src={ "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}/>
+             <h2>{name}</h2>
+             <h3 style={{fontStyle:"italic"}}>{cuisines.join(",")}</h3>
+             <h3>{avgRating}</h3>
+             <h4>{sla.deliveryTime} minutes</h4>
+           </div>
+        </>
+    )
+}
+const Body=()=>{
+    return(
+        <>
+            <div className="search"></div>
+            <div className="res-container">
+            {
+                resobj.map((i)=>{
+                   return  <Reataurentcard key={i.id} resData={i}/>
+                })
+            }
+            {/* <Reataurentcard image={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/woekdmnel2wvc9dpbmms" } resname={"The Biriyani Life"} cusine={"Biriyani,Lucknowi,mughal"} ratting={"4.2"} deliverytime={"30 "}/> */}
+            {/* <Reataurentcard image={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/4/22/5ccdabd4-2357-4ebd-823a-6092e8491166_1037841%20(1).jpg"}   resname={"Mr Phillys"}  cusine={"Burgur,American,Biriyani"} ratting={"4.5 stars"} deliverytime={20}/> */}
+            {/* <Reataurentcard image={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/a4ffed13eb197c6df43dfe1c756560e5"} resname={"Behrouj Biriyani"}  cusine={"North Indian ,punjabi,gujrati"} ratting={"4.3 stars"} deliverytime={35}/> */}
+            </div>
+        </>
+    )
+}
+const App=()=>{
+    return (
+        <>
+           <Header/>
+           <Body/>
+        </>
+    )
+}
+const root=reactDom.createRoot(document.getElementById("root"));
+root.render(<App/>)
